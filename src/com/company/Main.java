1fa62879e7a3ed47a206689ed7bb8867101b38e6
OwenSande <owensande07@gmail.com>
2022-04-12 15:26:09 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Staff employee = new Staff();
+        
+        employee.initializeSande56272();
+        employee.printSande56272();
+
+        employee.initialize1Sande56272();
+        employee.print1Sande56272();
+    }
+
+}
